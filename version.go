fb4656976cd151e7b1262aab673a5f12a57ef25b
icpd <35096485+icpd@users.noreply.github.com>
2023-06-24 10:23:40 +0800

    docs: adjust comment

--- version.go
+++ version.go
@@ -130,7 +130,7 @@ func (v VersionUpdateClause) ModifyStatement(stmt *gorm.Statement) {
 		}
 	}
 
-	// struct to map[string]interface{}. version field is int64, but need set its value to string
+	// convert struct to map[string]interface{}, we need to handle the version field with string, but which is an int64.
 	dv := reflect.ValueOf(stmt.Dest)
 	if reflect.Indirect(dv).Kind() == reflect.Struct {
 		selectColumns, restricted := stmt.SelectAndOmitColumns(false, true)
