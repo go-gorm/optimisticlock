f075eb9d5aa331ba880eba0749a9869d34ad22dc
icpd <35096485+icpd@users.noreply.github.com>
2024-07-24 01:33:50 +0800

    chore: cleanup dependencies

--- go.mod
+++ go.mod
@@ -3,27 +3,13 @@ module gorm.io/plugin/optimisticlock
 go 1.17
 
 require (
-	github.com/stretchr/testify v1.7.0
-	gorm.io/driver/postgres v1.3.1
 	gorm.io/driver/sqlite v1.2.6
 	gorm.io/gorm v1.23.8
 )
 
 require (
-	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
-	github.com/jackc/pgconn v1.10.1 // indirect
-	github.com/jackc/pgio v1.0.0 // indirect
-	github.com/jackc/pgpassfile v1.0.0 // indirect
-	github.com/jackc/pgproto3/v2 v2.2.0 // indirect
-	github.com/jackc/pgservicefile v0.0.0-20200714003250-2b9c44734f2b // indirect
-	github.com/jackc/pgtype v1.9.1 // indirect
-	github.com/jackc/pgx/v4 v4.14.1 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/mattn/go-sqlite3 v1.14.9 // indirect
-	github.com/pmezard/go-difflib v1.0.0 // indirect
-	golang.org/x/crypto v0.17.0 // indirect
-	golang.org/x/text v0.14.0 // indirect
 	gopkg.in/yaml.v3 v3.0.0 // indirect
 )
--- version_test.go
+++ version_test.go
@@ -4,19 +4,12 @@ import (
 	"database/sql/driver"
 	"encoding/json"
 	"fmt"
+	"strings"
 	"testing"
 	"time"
 
-	"github.com/stretchr/testify/require"
-	"gorm.io/driver/postgres"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
-)
-
-var (
-	mysqlDSN     = "gorm:gorm@tcp(localhost:9910)/gorm?charset=utf8&parseTime=True&loc=Local"
-	postgresDSN  = "user=gorm password=gorm dbname=gorm host=localhost port=9920 sslmode=disable TimeZone=Asia/Shanghai"
-	sqlserverDSN = "sqlserver://gorm:LoremIpsum86@localhost:9930?database=gorm"
 )
 
 type User struct {
@@ -29,107 +22,130 @@ type User struct {
 
 func TestVersion(t *testing.T) {
 	DB, err := gorm.Open(sqlite.Open(":memory:"), &gorm.Config{})
-	require.Nil(t, err)
+	if err != nil {
+		t.Fatalf("failed to connect to database: %v", err)
+	}
 
 	user := User{Name: "bob", Age: 20}
 	_ = DB.Migrator().DropTable(&User{})
 	_ = DB.AutoMigrate(&User{})
 	DB.Save(&user)
-	require.Equal(t, "bob", user.Name)
-	require.Equal(t, uint(20), user.Age)
-	require.Equal(t, int64(1), user.Version.Int64)
 
 	date := time.Date(2022, 1, 1, 0, 0, 0, 0, time.UTC)
 	DB.NowFunc = func() time.Time {
 		return date
 	}
+
 	rows := DB.Model(&user).Update("age", 18).RowsAffected
-	require.Equal(t, int64(1), rows)
-	require.Nil(t, DB.First(&user).Error)
-	require.Equal(t, int64(2), user.Version.Int64)
-	require.Equal(t, uint(18), user.Age)
-	require.Equal(t, date, user.UpdatedAt)
+	if rows != int64(1) {
+		t.Fatalf("expect RowsAffected 1, got %d", rows)
+	}
+
+	DB.First(&user)
+	if user.Version.Int64 != int64(2) || user.Age != uint(18) || user.UpdatedAt != date {
+		t.Fatalf("data does not match expectations, got %v", user)
+	}
+
 	DB.NowFunc = time.Now
 
 	rows = DB.Model(&user).Update("age", 16).RowsAffected
-	require.Equal(t, int64(1), rows)
-	require.Equal(t, uint(16), user.Age)
+	if rows != int64(1) || user.Age != uint(16) {
+		t.Fatalf("expect RowsAffected 1 and age 16, got RowsAffected %d and age %d", rows, user.Age)
+	}
 
+	// user version value not equal to database value
 	rows = DB.Model(&user).Update("age", 14).RowsAffected
-	require.Equal(t, int64(0), rows)
+	if rows != int64(0) {
+		t.Fatalf("expect RowsAffected 0, got %d", rows)
+	}
 
 	rows = DB.Model(&User{}).Where("id = 1").Update("age", 12).RowsAffected
-	require.Equal(t, int64(1), rows)
-	require.Nil(t, DB.First(&user).Error)
-	require.Equal(t, int64(4), user.Version.Int64)
-	require.Equal(t, uint(12), user.Age)
+	if rows != int64(1) {
+		t.Fatalf("expect RowsAffected 1, got %d", rows)
+	}
 
-	rows = DB.Model(&user).Select("name", "age", "version").Updates(&User{Name: "lewis"}).RowsAffected
-	require.Equal(t, int64(1), rows)
-	require.Nil(t, DB.First(&user).Error)
-	require.Equal(t, int64(5), user.Version.Int64)
-	require.Equal(t, "lewis", user.Name)
-	require.Equal(t, uint(0), user.Age)
+	DB.First(&user)
+	if user.Version.Int64 != int64(4) || user.Age != uint(12) {
+		t.Fatalf("expect version 4 and age 12, got version %v and age %d", user.Version.Int64, user.Age)
+	}
 
-	rows = DB.Model(&user).Updates(map[string]interface{}{
+	DB.Model(&user).Select("name", "age", "version").Updates(&User{Name: "lewis"})
+	DB.First(&user)
+	if user.Version.Int64 != int64(5) || user.Name != "lewis" || user.Age != uint(0) {
+		t.Fatalf("expect version 5, name lewis and age 0, got version %d, name %s and age %d", user.Version.Int64, user.Name, user.Age)
+	}
+
+	if rows = DB.Model(&user).Updates(map[string]interface{}{
 		"age": 18,
-	}).RowsAffected
-	require.Equal(t, int64(1), rows)
-	require.Nil(t, DB.First(&user).Error)
-	require.Equal(t, int64(6), user.Version.Int64)
-	require.Equal(t, uint(18), user.Age)
+	}).RowsAffected; rows != int64(1) {
+		t.Fatalf("expect RowsAffected == 1, got %d", rows)
+	}
+
+	DB.First(&user)
+	if user.Version.Int64 != int64(6) || user.Age != uint(18) {
+		t.Fatalf("expect version 6 and age 18, got version %d and age %d", user.Version.Int64, user.Age)
+	}
 
 	sql := DB.Session(&gorm.Session{DryRun: true}).Model(&user).Updates(nil).Statement.SQL.String()
-	require.Contains(t, sql, "`version`=`version`+1")
+	if !strings.Contains(sql, "`version`=`version`+1") {
+		t.Fatalf("invalid sql generated, got %v", sql)
+	}
+
+	sql = DB.Session(&gorm.Session{DryRun: true}).Model(&user).UpdateColumns(User{
+		Age: 18,
+	}).Statement.SQL.String()
+	if !strings.Contains(sql, "`version`=`version`+1") || !strings.Contains(sql, "`version` = ?") {
+		t.Fatalf("invalid sql generated, got %v", sql)
+	}
+
+	sql = DB.Session(&gorm.Session{DryRun: true}).Model(&user).UpdateColumns(map[string]interface{}{
+		"age": 18,
+	}).Statement.SQL.String()
+	if !strings.Contains(sql, "`version`=`version`+1") || !strings.Contains(sql, "`version` = ?") {
+		t.Fatalf("invalid sql generated, got %v", sql)
+	}
 
 	sql = DB.Session(&gorm.Session{DryRun: true}).Model(&user).Updates(&User{
 		Age:     19,
 		Version: Version{Int64: 10000000},
 	}).Statement.SQL.String()
-	require.Contains(t, sql, "`version`=`version`+1")
-
-	sql = DB.Session(&gorm.Session{DryRun: true}).Model(&user).UpdateColumns(User{
-		Age: 18,
-	}).Statement.SQL.String()
-	require.Contains(t, sql, "`version`=`version`+1")
-	require.Contains(t, sql, "`version` = ?")
-
-	sql = DB.Session(&gorm.Session{DryRun: true}).Model(&user).UpdateColumns(map[string]interface{}{
-		"age": 18,
-	}).Statement.SQL.String()
-	require.Contains(t, sql, "`version`=`version`+1")
-	require.Contains(t, sql, "`version` = ?")
+	if !strings.Contains(sql, "`version`=`version`+1") {
+		t.Fatalf("invalid sql generated, got %v", sql)
+	}
 
 	DB.NowFunc = func() time.Time {
 		return date
 	}
 	user.Name = "micky"
 	rows = DB.Updates(&user).RowsAffected
-	require.Equal(t, int64(1), rows)
-	require.Equal(t, date, user.UpdatedAt)
+	if rows != int64(1) || user.UpdatedAt != date {
+		t.Fatalf("expect RowsAffected 1 and date %s, got %d and %s", date, rows, user.UpdatedAt)
+	}
 	DB.NowFunc = time.Now
 
 	// Call Select method. Otherwise, will return primary key duplicate error.
 	user.Name = "lucky"
 	tx := DB.Select("*").Save(&user)
-	require.Nil(t, tx.Error)
-	require.Equal(t, int64(0), tx.RowsAffected)
+	if tx.Error != nil || tx.RowsAffected != int64(0) {
+		t.Fatalf("expect nil error and RowsAffected 0, got error %v and RowsAffected %d", tx.Error, tx.RowsAffected)
+	}
 
 	// support create
 	users := []User{{Name: "foo", Age: 30}, {Name: "bar", Age: 40, Version: Version{Int64: 100}}}
 	DB.Create(&users)
-	require.Equal(t, "foo", users[0].Name)
-	require.Equal(t, uint(30), users[0].Age)
-	require.Equal(t, int64(1), users[0].Version.Int64)
-	require.Equal(t, "bar", users[1].Name)
-	require.Equal(t, uint(40), users[1].Age)
-	require.Equal(t, int64(100), users[1].Version.Int64)
+	user0 := users[0]
+	user1 := users[1]
+	if user0.Name != "foo" || user0.Age != uint(30) || user0.Version.Int64 != int64(1) ||
+		user1.Name != "bar" || user1.Age != uint(40) || user1.Version.Int64 != int64(100) {
+		t.Fatalf("batch create failed, got %v", users)
+	}
 
 	// unscoped
 	user.Name = "foo"
 	r := DB.Unscoped().Updates(&user)
-	require.Equal(t, int64(1), r.RowsAffected)
-	require.Contains(t, sql, "`version`=`version`+1")
+	if r.RowsAffected != int64(1) {
+		t.Fatalf("unscoped update failed")
+	}
 }
 
 func TestJsonMarshal(t *testing.T) {
@@ -142,13 +158,21 @@ func TestJsonMarshal(t *testing.T) {
 		Version: Version{Int64: 12, Valid: true},
 	}
 	b, err := json.Marshal(user)
-	require.Nil(t, err)
-	require.Equal(t, `{"ID":1,"CreatedAt":"0001-01-01T00:00:00Z","UpdatedAt":"0001-01-01T00:00:00Z","DeletedAt":null,"Name":"lewis","Age":18,"Version":12}`, string(b))
+	if err != nil {
+		t.Fatalf("failed to marshal json: %v", err)
+	}
+	if string(b) != `{"ID":1,"CreatedAt":"0001-01-01T00:00:00Z","UpdatedAt":"0001-01-01T00:00:00Z","DeletedAt":null,"Name":"lewis","Age":18,"Version":12}` {
+		t.Fatalf("failed to marshal json, got %s", string(b))
+	}
 
 	user.Version.Valid = false
 	b, err = json.Marshal(user)
-	require.Nil(t, err)
-	require.Equal(t, `{"ID":1,"CreatedAt":"0001-01-01T00:00:00Z","UpdatedAt":"0001-01-01T00:00:00Z","DeletedAt":null,"Name":"lewis","Age":18,"Version":null}`, string(b))
+	if err != nil {
+		t.Fatalf("failed to marshal json: %v", err)
+	}
+	if string(b) != `{"ID":1,"CreatedAt":"0001-01-01T00:00:00Z","UpdatedAt":"0001-01-01T00:00:00Z","DeletedAt":null,"Name":"lewis","Age":18,"Version":null}` {
+		t.Fatalf("failed to marshal json, got %s", string(b))
+	}
 }
 
 type Ext struct {
@@ -195,7 +219,9 @@ func (e Ext) Value() (driver.Value, error) {
 
 func TestEmbed(t *testing.T) {
 	DB, err := gorm.Open(sqlite.Open(":memory:"), &gorm.Config{})
-	require.Nil(t, err)
+	if err != nil {
+		t.Fatalf("failed to connect to database: %v", err)
+	}
 
 	user := User{Name: "bob", Age: 20}
 	_ = DB.Migrator().DropTable(&User{})
@@ -210,56 +236,47 @@ func TestEmbed(t *testing.T) {
 	}
 	_ = DB.Migrator().DropTable(&Account{})
 	_ = DB.AutoMigrate(&Account{})
-	require.Nil(t, DB.Save(&account).Error)
-	require.Nil(t, DB.Save(&account).Error)
+	if err = DB.Save(&account).Error; err != nil {
+		t.Fatalf("failed to save account: %v", err)
+	}
 
 	sql := DB.Session(&gorm.Session{DryRun: true}).Updates(&account).Statement.SQL.String()
-	require.Contains(t, sql, "`updated_at`=?")
-	require.Contains(t, sql, "`version`=`version`+1")
-	require.Contains(t, sql, "`user_id`=?")
-	require.Contains(t, sql, "`ext`=?")
-	require.Contains(t, sql, "`amount`=?")
+	if !strings.Contains(sql, "`updated_at`=?") ||
+		!strings.Contains(sql, "`version`=`version`+1") ||
+		!strings.Contains(sql, "`user_id`=?") ||
+		!strings.Contains(sql, "`ext`=?") ||
+		!strings.Contains(sql, "`amount`=?") {
+		t.Fatalf("invalid sql generated, got %v", sql)
+	}
 
 	var a Account
 	DB.First(&a)
 
 	a.Amount = 233
-	err = DB.Updates(&a).Error
-	require.Nil(t, err)
+	rows := DB.Updates(&a).RowsAffected
+	if rows != int64(1) {
+		t.Fatalf("expect RowsAffected 1, got %d", rows)
+	}
 
 	a.Amount = 556
-	rows := DB.Updates(&a).RowsAffected
-	require.Equal(t, int64(0), rows)
-	require.Nil(t, DB.First(&a).Error)
-	require.Equal(t, 233, a.Amount)
+	rows = DB.Updates(&a).RowsAffected
+	if rows != int64(0) {
+		t.Fatalf("expect RowsAffected 0, got %d", rows)
+	}
+	DB.First(&a)
+	if a.Amount != 233 {
+		t.Fatalf("expect 233, got %d", a.Amount)
+	}
 
 	a.Amount = 9999
-	rows = DB.Updates(&a).RowsAffected
-	require.Equal(t, int64(1), rows)
+	if rows = DB.Updates(&a).RowsAffected; rows != int64(1) {
+		t.Fatalf("expect RowsAffected 1, got %d", rows)
+	}
 
 	var a1 Account
-	require.Nil(t, DB.First(&a1).Error)
-	require.Equal(t, a.Amount, a1.Amount)
-	require.Equal(t, int64(3), a.Version.Int64)
-	require.Equal(t, int64(4), a1.Version.Int64)
-}
-
-// use gorm.io/gorm/tests docker compose file
-func TestPostgres(t *testing.T) {
-	DB, err := gorm.Open(postgres.Open(postgresDSN), &gorm.Config{})
-	require.Nil(t, err)
-
-	user := User{Name: "bob", Age: 20}
-	_ = DB.Migrator().DropTable(&User{})
-	_ = DB.AutoMigrate(&User{})
-	DB.Save(&user)
-	require.Equal(t, "bob", user.Name)
-	require.Equal(t, uint(20), user.Age)
-	require.Equal(t, int64(1), user.Version.Int64)
-
-	rows := DB.Model(&user).Update("age", 18).RowsAffected
-	require.Equal(t, int64(1), rows)
-	require.Nil(t, DB.First(&user).Error)
-	require.Equal(t, int64(2), user.Version.Int64)
-	require.Equal(t, uint(18), user.Age)
+	DB.First(&a1)
+	if a.Amount != a1.Amount || a.Version.Int64 != int64(2) || a1.Version.Int64 != int64(3) {
+		t.Fatalf("got a.Amount %v, a1.Amount %v, a.Version %v, a1.Version %v", a.Amount, a1.Amount,
+			a.Version.Int64, a1.Version.Int64)
+	}
 }
